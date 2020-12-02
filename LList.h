ddb4e981774d1963391d62d6df8f2136ce54fb07
lacho55 <lacho_fitness@abv.bg>
2020-12-02 21:04:59 +0200

    Creating iterator for singly linked list.

--- LList.h
+++ LList.h
@@ -14,6 +14,7 @@ struct Node {
 };
 
 
+/* -------- Iterator -------- */
 template<typename>
 class LListIterator;
 
@@ -46,3 +47,84 @@ void LList<T>::copy(const LList<T>& other) {
 /* ------- Public Methods --------- */
 template<typename T>
 LList<T>::LList() : head(nullptr), tail(nullptr) {}
+
+
+
+
+/* ------- Iterator -------*/
+template<typename T>
+class LListIterator {
+private:
+	Node<T>* ptr;
+
+public:
+	/* ------ Public Iterator Methods -------- */
+	friend class LList<T>;
+	LListIterator(Node<T>* newPtr = nullptr) : ptr(newPtr) {}
+	LListIterator<T> next() const;
+	LListIterator<T> prev() const;
+	T& get() const;
+	bool valid() const;
+	bool operator==(const LListIterator<T>&) const;
+	bool operator!=(const LListIterator<T>&) const;
+	T& operator*() const;
+	LListIterator<T> operator++(int);
+	LListIterator<T>& operator++();
+	operator bool() {
+		return valid();
+	}
+};
+
+
+/* -------- Public Iterator Methods ------- */
+template<typename T>
+LListIterator<T> LListIterator<T>::next() const {
+	return LListIterator(ptr->next);
+}
+
+
+template<typename T>
+T& LListIterator<T>::get() const {
+	return ptr->data;
+}
+
+
+template<typename T>
+bool LListIterator<T>::valid() const {
+	return (ptr != nullptr);
+}
+
+
+template<typename T>
+bool LListIterator<T>::operator==(const LListIterator<T>& iter) const{
+	return (ptr == iter.ptr);
+}
+
+
+template<typename T>
+bool LListIterator<T>::operator!=(const LListIterator<T>& iter) const {
+	return !(*this == iter);
+}
+
+
+template<typename T>
+T& LListIterator<T>::operator*() const {
+	return get();
+}
+
+
+template<typename T>
+LListIterator<T> LListIterator<T>::operator++(int) {
+	LListIterator prevData = *this;
+	*this = next();
+	
+	return prevData;
+}
+
+
+template<typename T>
+LListIterator<T>& LListIterator<T>::operator++() {
+	*this = next();
+	
+	return *this;
+}
