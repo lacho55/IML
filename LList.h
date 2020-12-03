0028defcf3d210415980556bbd45bd19f719b981
lacho55 <lacho_fitness@abv.bg>
2020-12-03 11:32:56 +0200

    Finishing Linked List implementation.

--- LList.h
+++ LList.h
@@ -1,6 +1,6 @@
 #pragma once
 #include<iostream>
-
+#include<fstream>
 
 template<typename T>
 struct Node {
@@ -39,8 +39,33 @@ public:
 	bool insertAfter(const T&, LListIterator<T>);
 	bool insertBefore(const T&, LListIterator<T>);
 	bool deleteAfter(T&, LListIterator<T>);
+	bool deleteAt(T&, LListIterator<T>);
+	bool deleteBefore(T&, LListIterator<T>);
+	T& getAt(LListIterator<T>) const;
+	void insertBegin(const T&);
 	void insertTail(const T&);
+	bool deleteBegin(T&);
 	bool deleteTail(T&);
+	LList<T>& operator+=(const T&);
+	void print(std::ostream& out = cout) const {
+		out << '(';
+		for (LListIterator<T> iter = begin(); iter; ++iter) {
+			out << *iter;
+			if (iter != end()) {
+				out << ',';
+			}
+		}
+		out << ')';
+	}
+	void append(LList<T>&);
+	int length() const;
+	void reverse();
+	void unique();
+	void append(LList<T>&, const LList<T>&);
+	void reverse(LList<T>&);
+	void split(const LList<T>&, LList<T>&, LList<T>&);
+	LList<T> merge(const LList<T>&, const LList<T>&);
+	void mergeSort(LList<T>&);
 	
 
 	LListIterator<T> begin() const;
@@ -191,14 +216,222 @@ bool LList<T>::deleteAfter(T& toDel, LListIterator<T> iter) {
 
 
 template<typename T>
+bool LList<T>::deleteAt(T& toDel, LListIterator<T> iter) {
+	if (!iter || empty()) {
+		return false;
+	}
+
+	if (it == begin()) {
+		Node<T>* tmpDel = head;
+		head = head->next;
+
+		if (head == nullptr) {
+			tail = nullptr;
+		}
+
+		toDel = tmpDel->data;
+		delete tmpDel;
+		return true;
+	}
+
+	return deleteAfter(x, findPrevious(iter));
+}
+
+
+template<typename T>
+bool LList<T>::deleteBefore(T& toDel, LListIterator<T> iter) {
+	if (!iter || iter == begin() || empty) {
+		return false;
+	}
+
+	//It is a valid iterator
+	//Is is not pointing to the first element in the list
+	//There are at least two elements in the list
+
+	deleteAt(toDel, findPrevious(iter));
+}
+
+
+template<typename T>
+T& LList<T>::getAt(LListIterator<T> iter) const {
+	return (iter.get());
+}
+
+
+template<typename T>
+void LList<T>::insertBegin(const T& newElem) {
+	insertBefore(newElem, begin());
+}
+
+
+template<typename T>
 void LList<T>::insertTail(const T& newElem) {
 	insertAfter(newElem, end());
 }
 
 
 template<typename T>
+bool LList<T>::deleteBegin(T& toDel) {
+	return deleteAt(toDel, begin());
+}
+
+
+template<typename T>
 bool LList<T>::deleteTail(T& toDel) {
 	return deleteAt(toDel, end());
+}
+
+
+template<typename T>
+LList<T>& LList<T>::operator+=(const T& newData) {
+	insertTail(newData);
+	return *this;
+}
+
+
+template<typename T>
+void LList<T>::append(LList<T>& other) {
+	if (!empty()) {
+		tail->next = other.head;
+		if (!ther.empty()) {
+			tail = other.tail;
+		}
+	}
+	else {
+		head = other.head;
+		tail = other.tail;
+	}
+
+	other.head = nullptr;
+	other.tail = nullptr;
+}
+
+
+template<typename T>
+int LList<T>::length() const {
+	int length = 0;
+	for (LListIterator<T> iter = begin(); iter; ++iter) {
+		length++;
+	}
+
+	return length;
+}
+
+
+template<typename T>
+void LList<T>::reverse() {
+	if (empty() || head->next == nullptr) {
+		return;
+	}
+
+	Node<T>* prev = head;
+	Node<T>* nextElem = head->next;
+	tail = head;
+	tail->next = nullptr;
+
+	while (nextElem != nullptr) {
+		head = nextElem;
+		nextElem = nextElem->next;
+		head->next = prev;
+		prev = head;
+	}
+}
+
+
+template<typename T>
+void LList<T>::unique() { //A methods for removing duplicates and turning the list into a set
+	T helper;
+
+	for (LListIterator<T> firstIter = begin(); firstIter; ++firstIter) {
+		for (LListIterator<T> secondIter = firstIter; secondIter.next(); ) {
+			if (*firstIter == *(secondIter.next())) {
+				deleteAfter(helper, secondIter);
+			}
+			else {
+				secondIter++;
+			}
+		}
+	}
+}
+
+
+template<typename T>
+void LList<T>::append(LList<T>& list1, const LList<T>& list2) {
+	for (LListIterator<T> iter = list2.begin(); iter; ++iter) {
+		list1 += *iter;
+	}
+}
+
+
+template<typename T>
+void LList<T>::reverse(LList<T>& list) {
+	LListIterator<T> iter = list.begin();
+
+	T helper;
+
+	while (list.deleteAfter(helper, iter)) {
+		list.insertBegin(helper);
+	}
+}
+
+
+template<typename T>
+void LList<T>::split(const LList<T>& list, LList<T>& list1, LList<T>& list2) {
+	LList<T>* addFirst = &list1;
+	LLisT<T>* addLast = &list2;
+
+	for (LListIterator<T> iter = list.begin(); iter; ++iter) {
+		(*addFirst).insertTail(*iter);
+		swap(addFirst, addLast);
+	}
+}
+
+
+template<typename T>
+LList<T> LList<T>::merge(const LList<T>& list1, const LList<T>& list2) {
+	LList<T> sorted;
+	LListIterator<T> iter1 = list1.begin();
+	LListIterator<T> iter2 = list2.begin();
+
+	while (iter1 && iter2) {
+		if (*iter1 <= *iter2) {
+			sorted += *iter1;
+			iter1++;
+		}
+		else {
+			sorted += *iter2;
+			iter2++;
+		}
+	}
+
+	while (iter1) {
+		sorted += *iter1;
+		iter1++;
+	}
+	while (iter2) {
+		sorted += *iter2;
+		iter2++;
+	}
+
+	return sorted;
+}
+
+
+template<typename T>
+void LList<T>::mergeSort(LList<T>& list) {
+	if (list.begin() == list.end()) {
+		return;
+	}
+
+	LList<T> list1;
+	LList<T> list2;
+
+	split(list, list1, list2);
+
+	mergeSort(list1);
+	mergeSort(lsit2);
+
+	list = merge(list1, list2);
 }
 
 
