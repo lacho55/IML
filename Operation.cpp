97a8d032316a0b46ff0b31d74535a6323bd3e395
lacho55 <lacho_fitness@abv.bg>
2020-12-09 20:47:22 +0200

    Realizations of some vital methods

--- Operation.cpp
+++ Operation.cpp
@@ -1,5 +1,6 @@
 #include "Operation.h"
-
+#include <string>
+#include <sstream>
 
 /* --------- Public Methods and Constructors ---------*/
 Operation::Operation() {
@@ -30,6 +31,56 @@ void Operation::addToList(double newData) {
 //Appends the new list at the end of the current list
 void Operation::appendList(LList<double>& other) {
 	list.append(other);
+}
+
+
+void Operation::applyGivenOperation() {
+
+	if (operationDone) {
+		return;
+	}
+
+	//taking only the type of the operation without anything other
+	std::string oprTobeGiven = (operation.getOperation()).substr(0, 3);
+
+	if (oprTobeGiven == "MAP") {
+		//apply map operation
+	}
+	if (oprTobeGiven == "AGG") {
+		//apply agg operation
+	}
+	if (oprTobeGiven == "STR") {
+		//apply SRT
+	}
+
+	operationDone = true;
+}
+
+
+//Returns the final list
+LList<double> Operation::getResAsList() {
+	return list;
+}
+
+
+//Returns the resulting list as a whole string
+std::string Operation::getResAsString() {
+	
+	std::string finalRes;
+	std::stringstream outputStream;
+
+	for (LListIterator<double> iter = list.begin(); iter; ++iter) {
+
+		outputStream << *iter;
+
+		if (iter != list.end()) {
+			outputStream << " ";
+		}
+	}
+
+	finalRes = outputStream.str();
+
+	return finalRes;
 }
 
 
