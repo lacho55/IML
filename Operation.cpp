a48d0bbc41896945e34ae3c19f67ea40412f2b4a
lacho55 <lacho_fitness@abv.bg>
2020-12-09 17:15:42 +0200

    Realization of two constructors and one external functions + appendList and addToList.

--- Operation.cpp
+++ Operation.cpp
@@ -2,3 +2,43 @@
 
 
 /* --------- Public Methods and Constructors ---------*/
+Operation::Operation() {
+	operationDone = false;
+}
+
+
+Operation::Operation(std::string newOperation, std::string newAttribute) {
+
+	operationDone = false;
+	operation.setOperation(newOperation);
+
+	//Checks if there is an attribute
+	if (newAttribute.length() > 0) {
+		operation.setAttribute(newAttribute);
+		operation.setCheckForAttribute(true);
+	}
+
+}
+
+
+//Adds new element at the end of the list of number
+void Operation::addToList(double newData) {
+	list.insertTail(newData);
+}
+
+
+//Appends the new list at the end of the current list
+void Operation::appendList(LList<double>& other) {
+	list.append(other);
+}
+
+
+/* --------- External Functions ------- */
+double add(double num1, double num2) {
+	return (num1 + num2);
+}
+
+
+double mul(double num1, double num2) {
+	return (num1 * num2);
+}
