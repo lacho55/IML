4164c4b604b17ded9e3dc3126b9b9ea0f2bebcd8
lacho55 <lacho_fitness@abv.bg>
2021-01-14 09:45:31 +0200

    Finishing operation class

--- Operation.cpp
+++ Operation.cpp
@@ -2,7 +2,111 @@
 #include <string>
 #include <sstream>
 
-/* --------- Public Methods and Constructors ---------*/
+/* ------ Private Methods ------- */
+void Operation::applyMAP() {
+
+	//Taking the exact type of MAP operation wether it is INC or MLT
+	std::string typeOfMap = (operation.getOperation()).substr(4, 3);
+
+	//writing it that way because we have only two types of functions
+	//and both of them will take two doule parameters and return double value
+	double(*func)(double, double);
+
+	typeOfMap == "INC" ? func = add : func = mul;
+
+	//Just to store the needed parameter of the given function
+	std::stringstream paramReader;
+	paramReader << operation.getAttribute();
+
+	double param;
+	paramReader >> param;
+
+	//Incrementing or multiplying all elements of the list 
+	for (LListIterator<double> iter = list.begin(); iter; iter++) {
+		*iter = func(*iter, param);
+	}
+
+}
+
+
+//Aggregation operation
+void Operation::applyAGG() {
+
+	//Getting the type of the aggregate operation
+	std::string typeOfAGG = operation.getOperation().substr(4, 3);
+	double helper;
+
+	if (typeOfAGG == "FST") {
+		while (list.begin() != list.end())
+			list.deleteEnd(helper);
+		return;
+	}
+
+	if (typeOfAGG == "LST") {
+		while (list.begin() != list.end())
+			list.deleteBegin(helper);
+		return;
+	}
+
+	// the operation is sum, product and average 
+	double(*func)(double, double);
+
+	LList<double> finalResult;
+
+	if (typeOfAGG == "PRO") {
+		func = mul;
+		helper = 1;
+	}
+	else {
+		func = add;
+		helper = 0;
+	}
+
+	for (LListIterator<double> iter = list.begin(); iter; iter++)
+		helper = func(helper, *iter);
+
+	if (typeOfAGG == "AVG") {
+		helper /= list.length();
+	}
+
+	finalResult.insertEnd(helper);
+	list = finalResult;
+}
+
+
+//Sorting operation
+void Operation::applySRT() {
+
+	std::string typeOfSRT = operation.getOperation().substr(4, 3);
+
+	if (typeOfSRT == "DST") {
+		list.unique();
+		return;
+	}
+
+	if (typeOfSRT == "ORD") {
+		mergeSort(list);
+	}
+
+	if (typeOfSRT == "REV" || operation.getAttribute() == "DSC")
+		list.reverse();
+
+	if (typeOfSRT != "SLC")
+		return;
+
+	std::stringstream reader;
+	double helper;
+	int index;
+
+	reader << operation.getAttribute();
+	reader >> index;
+
+	for (int i = 0; i < index; i++)
+		list.deleteBegin(helper);
+}
+
+
+/* ------- Public Methods and Constructors ------- */
 Operation::Operation() {
 	operationDone = false;
 }
@@ -18,13 +122,12 @@ Operation::Operation(std::string newOperation, std::string newAttribute) {
 		operation.setAttribute(newAttribute);
 		operation.setCheckForAttribute(true);
 	}
-
 }
 
 
 //Adds new element at the end of the list of number
-void Operation::addToList(double newData) {
-	list.insertTail(newData);
+void Operation::addToList(double newData) { 
+	list.insertEnd(newData);
 }
 
 
@@ -34,23 +137,23 @@ void Operation::appendList(LList<double>& other) {
 }
 
 
+//A method that applies its given operation from the three types
 void Operation::applyGivenOperation() {
 
 	if (operationDone) {
 		return;
 	}
-
+	
 	//taking only the type of the operation without anything other
 	std::string oprTobeGiven = (operation.getOperation()).substr(0, 3);
-
 	if (oprTobeGiven == "MAP") {
-		//apply map operation
+		applyMAP();
 	}
 	if (oprTobeGiven == "AGG") {
-		//apply agg operation
+		applyAGG();
 	}
-	if (oprTobeGiven == "STR") {
-		//apply SRT
+	if (oprTobeGiven == "SRT") {
+		applySRT();
 	}
 
 	operationDone = true;
@@ -65,11 +168,11 @@ LList<double> Operation::getResAsList() {
 
 //Returns the resulting list as a whole string
 std::string Operation::getResAsString() {
-	
+
 	std::string finalRes;
 	std::stringstream outputStream;
 
-	for (LListIterator<double> iter = list.begin(); iter; ++iter) {
+	for (LListIterator<double> iter = list.begin(); iter; iter++) {
 
 		outputStream << *iter;
 
@@ -79,17 +182,15 @@ std::string Operation::getResAsString() {
 	}
 
 	finalRes = outputStream.str();
-
 	return finalRes;
 }
 
 
-/* --------- External Functions ------- */
+/* -------- External Functions ------- */
 double add(double num1, double num2) {
-	return (num1 + num2);
+	return num1 + num2;
 }
 
-
 double mul(double num1, double num2) {
-	return (num1 * num2);
+	return num1 * num2;
 }
--- Operation.h
+++ Operation.h
@@ -1,12 +1,11 @@
 #pragma once
 #include "LList.h"
-#include "Tag.h"
+#include "tag.h"
 
-class Operation
-{
+class Operation {
 private:
 	LList<double> list;
-	Tag operation;
+	Tag operation; 
 	bool operationDone; //It acts like a switch depending on operation's execution
 
 	/* ------ Private Methods ------- */
@@ -14,19 +13,18 @@ private:
 	void applyAGG();
 	void applySRT();
 
-
 public:
 	/* ------- Public Methods and Constructors ------- */
 	Operation();
-	Operation(std::string newOperation, std::string newAttribute = "");
+	Operation(std::string operation, std::string attribute = "");
 
-	void addToList(double);
+	void addToList(double data);
 	void appendList(LList<double>&);
 	void applyGivenOperation();
 
+
 	LList<double> getResAsList();
 	std::string getResAsString();
-
 };
 
 
